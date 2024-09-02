e03b20f4a5b986d3cdff2dcfa9930fdac58fccfd
Ricky Chris <rickychris6@gmail.com>
2024-09-02 06:38:08 +0400

    hbs templated for web-server

--- src/app.js
+++ src/app.js
@@ -1,24 +1,66 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
 app.get('', (req, res) => {
-    res.send('Hello World!');
-
-})
-
-app.get('/help', (req, res) => {
-    res.send('Help page');
+    res.render('index', {
+        title: 'Weather',
+        name: 'Chris'
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.render('about', {
+        title: 'About me',
+        name: 'Chris'
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This is a helpful message',
+        name: 'Chris'
+    });
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page');
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    });
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Chris',
+        errorMessage: 'Help article not found'
+    });
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Chris',
+        errorMessage: 'Page not found'
+    });
+})
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
