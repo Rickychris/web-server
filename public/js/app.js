824d0c2859e6b30336b30b0581574c4590423ead
Ricky Chris <rickychris6@gmail.com>
2024-09-03 07:07:00 +0400

    api integration with frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
 console.log('Client side javascript file is loaded');
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const search = document.querySelector('input').value
+    document.querySelector('input').value = ''
+    document.querySelector('div#weather').textContent = 'Loading...'
+    fetch('/weather?address=' + search).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                document.querySelector('div#weather').textContent = data.error
+                document.querySelector('div#location').textContent = ''
+            } else {
+                document.querySelector('div#weather').textContent = data.forecast
+                document.querySelector('div#location').textContent = data.location
+            }
+        })
+    })
+})
